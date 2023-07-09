71d2d7d15e3ac8ebc3cc8fb813258b7d42438111
unknown <rlaghdtlr012@naver.com>
2023-07-10 00:49:22 +0900

    [FEAT] 회원 도메인 & 리포지토리 개발 및 TC 작성

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,61 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    // 각 테스트케이스들은 순서가 보장되지 않는다. 각 테스트 케이스들은 서로 의존 관계를 가질 수도 있다.
+    // 따라서 테스트 케이스는 각각 독립적으로 실행돼야 함으로 각 테스트 케이스들이 끝나면 데이터를 clear 해줘야 한다.
+    // 각 테스트 케이스들이 끝나고 실행되는 callback 함수 같은 느낌.
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+        // System.out.println("result = " + (result == member));
+        // Assertions.assertEquals(member, result);
+        Assertions.assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        Assertions.assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        Assertions.assertThat(result.size()).isEqualTo(2);
+    }
+
+}
