f57f48c9da221b4b8d9c9e0ae6ffe368c2a6ec23
unknown <rlaghdtlr012@naver.com>
2023-07-12 22:17:08 +0900

    스프링 통합테스트, jdbcTemplate

--- 01_스프링입문/src/main/java/hello/hellospring/SpringConfig.java
+++ 01_스프링입문/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,20 +1,28 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration // 자바 코드로 직접 스프링 빈 등록하기
 public class SpringConfig {
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
-        // MemberRepository는 구현체이기 때문에 class인 MemoryMemberRepository로 생성
-        return new MemoryMemberRepository();
+//        MemberRepository는 구현체이기 때문에 class인 MemoryMemberRepository로 생성
+//        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
