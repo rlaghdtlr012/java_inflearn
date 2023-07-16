4363857848e9536a65142166e6a8dddf22f1d84d
unknown <rlaghdtlr012@naver.com>
2023-07-17 02:57:38 +0900

    AppConfig를 사용한 관심사 분리 및 스프링으로 전환 시작

--- 02_스프링핵심원리_기본편/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 02_스프링핵심원리_기본편/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,21 +1,23 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-
 //    DIP를 만족시키기 위해 인터페이스에만 의존하도록 코드를 수정
 //    아래 코드와 같이 코드를 짠다면 NullPoint Exception이 나기 때문에 이 문제를 해결하기 위해서는 누군가가 클라이언트의 OrderServiceImpl에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해주어야 한다.
     private DiscountPolicy discountPolicy;
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
