de1c11c4852b733d952a29303223f682a1caa4f1
unknown <rlaghdtlr012@naver.com>
2023-07-27 23:55:58 +0900

    Provider를 이용하여 프로토타입 빈과 싱글톤 빈을 같이 사용할 때 생기는 문제점 해결

--- 02_스프링핵심원리_기본편/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ 02_스프링핵심원리_기본편/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -32,19 +34,23 @@ public class SingletonWithPrototypeTest1 {
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
         // prototypeBean은 하나를 가지고 같이 쓰기 때문에 새로 만든 clientBean2일지라도 count는 2이다.
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입. 그래서 생성 시점에 주입된 같은 빈을 계속 같이 씀
+//        private final PrototypeBean prototypeBean; // 생성 시점에 주입. 그래서 생성 시점에 주입된 같은 빈을 계속 같이 씀
+
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
