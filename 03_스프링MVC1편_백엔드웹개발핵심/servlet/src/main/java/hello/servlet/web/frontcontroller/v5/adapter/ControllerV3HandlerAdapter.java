450ee7faf4a65c61a253143e5a8d6de4d430d196
unknown <rlaghdtlr012@naver.com>
2023-08-17 23:11:28 +0900

    temp

--- 03_스프링MVC1편_백엔드웹개발핵심/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
+++ 03_스프링MVC1편_백엔드웹개발핵심/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
     @Override
     public boolean supports(Object handler) {
+//        return aaa
         return (handler instanceof ControllerV3);
     }
 
