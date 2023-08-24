8bb3081d958e33366419b3459cde2f834c9fd171
unknown <rlaghdtlr012@naver.com>
2023-08-24 21:42:16 +0900

    mvc 기본 기능. 프로젝트 및 로깅 생성

---
+++ 03_스프링MVC1편_백엔드웹개발핵심/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+    // Lombok(@Slf4j)을 사용하면 바로 밑 13 line의 코드를 이미 작성한 것과 똑같은 효과를 볼 수 있음 
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+        // 밑으로 갈 수록 레벨이 높아지고 중요도가 높아지는거
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+        return "ok";
+    }
+}
