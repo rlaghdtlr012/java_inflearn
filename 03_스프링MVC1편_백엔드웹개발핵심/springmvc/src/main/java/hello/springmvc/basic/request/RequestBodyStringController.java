6d3cd03993a17a2cdd5fb720e2db64ee9608f8ae
unknown <rlaghdtlr012@naver.com>
2023-09-04 23:26:20 +0900

    Http 요청 메시지-JSON

--- 03_스프링MVC1편_백엔드웹개발핵심/springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ 03_스프링MVC1편_백엔드웹개발핵심/springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -1,14 +1,19 @@
 package hello.springmvc.basic.request;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 
 @Controller
@@ -22,5 +27,28 @@ public class RequestBodyStringController {
 
         log.info("messageBody={}", messageBody);
         response.getWriter().write("ok");
+    }
+
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+        responseWriter.write("ok");
+    }
+
+    @PostMapping("/request-body-string-v3") // HttpEntity => Http 요청 안에 있는 것이 문자 임을 알고, Body에 있는 요청 메시지를 문자로 바꿔서 넣어준다
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
+        String messageBody = httpEntity.getBody();
+        log.info("messageBody={}", messageBody);
+        return new HttpEntity<>("ok");
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-string-v4") // 어노테이션을 이용한 mapping (Http Body를 읽어서 바로 갖다 쓰게 해 줌. 완전 편하다)
+    public String requestBodyStringV4(@RequestBody String messageBody) throws IOException {
+        // ******* @RequestBody를 실무에서 많이 쓰는 편 **********
+        log.info("messageBody={}", messageBody);
+        return "ok";
     }
 }
