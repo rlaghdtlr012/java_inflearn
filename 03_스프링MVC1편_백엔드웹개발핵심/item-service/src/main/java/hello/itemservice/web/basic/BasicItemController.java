6f6f5be696e3e7edaccd20a29df041f12312d181
rlaghdtlr012 <rlaghdtlr012@naver.com>
2023-09-08 22:17:08 +0900

    상품관련 HTML 추가 및 타임리프 컨버팅 진행중

---
+++ 03_스프링MVC1편_백엔드웹개발핵심/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,40 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor // 밑에 주석 처리 된 @Autowired 의존성 주입이랑 같은 기능 제공
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @Autowired
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/item";
+    }
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 100));
+        itemRepository.save(new Item("itemB", 20000, 200));
+    }
+
+}
