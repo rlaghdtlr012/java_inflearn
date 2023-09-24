c1b231184f42d40c37911e7433fc19188da553e6
rlaghdtlr012 <rlaghdtlr012@naver.com>
2023-09-24 10:00:00 +0900

    트랜잭션 템플릿을 통한 반복로직 최소화

---
+++ 05_스프링DB1편_데이터접근핵심원리/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -0,0 +1,63 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 템플릿을 활용한 중복 코드 제거
+ */
+@Slf4j
+public class MemberServiceV3_2 {
+    private final TransactionTemplate txTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
+    // DataSource를 직접 사용하는 것이 아닌 플랫폼 트랜잭션 매니저를 주입하여 사용한다
+    // private final DataSource dataSource;
+    // private final PlatformTransactionManager transactionManager;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // txTemplate 안에서 오류 터지면 롤백, 잘 실행되면 커밋을 알아서 해준다.
+        txTemplate.executeWithoutResult((status) -> {
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+        // 트랜잭션 시작
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            // 비즈니스 로직 시작
+
+            transactionManager.commit(status); // 성공하면 커밋
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        if (toMember.getMemberId().equals("ex")) { // 일부러 오류 발생 시킨거
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+}
