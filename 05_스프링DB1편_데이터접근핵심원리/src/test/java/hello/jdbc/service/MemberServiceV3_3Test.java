450b6061032a62dc9dad14a10d560307edabc742
rlaghdtlr012 <rlaghdtlr012@naver.com>
2023-09-28 23:21:26 +0900

    트랜잭션 AOP 적용

--- 05_스프링DB1편_데이터접근핵심원리/src/test/java/hello/jdbc/service/MemberServiceV3_3Test.java
+++ 05_스프링DB1편_데이터접근핵심원리/src/test/java/hello/jdbc/service/MemberServiceV3_3Test.java
@@ -20,7 +20,7 @@ import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 /**
- * 트랜잭션 - @Transactional AOP
+ * 트랜잭션 - @Transactional AOP 적용
  */
 @Slf4j
 @SpringBootTest
